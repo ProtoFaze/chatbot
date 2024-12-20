34266b2ad547471175b4ebeb945e283e717fc8ee
DamonNg <110209956+ProtoFaze@users.noreply.github.com>
2024-12-20 11:51:10 +0800

    REFACTOR: shortened code, added chatlog format update before analysis

--- src/pages/Dashboard.py
+++ src/pages/Dashboard.py
@@ -1,109 +1,114 @@
-import os
 import datetime
 import streamlit as st
-from bson import objectid as oid
-from shared.Connection import setup_LLM, setup_mongo
-from shared.Analysis import AnalysisResults
-from dotenv import load_dotenv
-load_dotenv()
-
-@st.dialog("Chatlog Already Analysed")
-def show_analysis_dialog():
-    st.write("This chatlog has already been analysed.")
+from shared.Setup import setup_LLM, setup_mongo, initialize_streamlit_session
+from shared.Models import AnalysisResults
+initialize_streamlit_session()
+st.set_page_config(page_title="Dashboard", page_icon=":material/dashboard:",layout="wide", menu_items={"about":"#Dashboard with a simple chatlog analysis function ,locked behind admin credentials"})
+@st.dialog("Chatlog will not be analysed")
+def show_analysis_dialog(chat_record):
+    if len(chat_record['chatlog'])>2:
+        st.write("This chatlog has already been analysed.")
+    else:
+        st.write("Analysis blocked, lack of messages would waste resources")
     if st.button("Close"):
         st.rerun()
 
 def show_chat_details(item: dict):
-    st.session_state['inspecting_chat_collection'] = dict(item)
+    st.session_state['inspecting_chat_collection'] = st.session_state['mongo_client'][st.session_state["MONGODB_DB"]]['chat_session'].find_one({'_id': item['_id']})
 
-def analyse_chatlog(chat_collection):
+def analyse_chatlog(chat_record):
     if 'llm_client' not in st.session_state:
         llm_client = setup_LLM()
     else:
         llm_client = st.session_state['llm_client']
+    chatlog = ''
+    with st.spinner("Preprocessing chatlog"):
+        for seq,message in enumerate(chat_record['chatlog']):
+            if message['role'] == 'user':
+                chatlog += str(seq)+". { USER_MSG:"+message['content']+"},\n"
+            else:
+                chatlog +=  str(seq)+". { AI_MSG:"+message['content']+"},\n"
+        print(chatlog)
     with st.spinner("Analyzing chatlog"):
-        chatlog = chat_collection['chatlog']
+        prompt = f"""{chatlog}
 
-        prompt = f'Analyse the chatlog: {chatlog}'
+Analyse the chatlog, tell me the overall user sentiment, overal user interest in the product the AI is advertising, key topics in the conversation, is the user not interested in signing up, and the disengagement reason.
+please reply in json format.
+"""
         response = llm_client.generate(model = 'llama3.2',
                                        prompt = prompt,
                                        format = AnalysisResults.model_json_schema(),
                                        options={'temperature':0},
-                                       stream=False)
+                                       stream=False
+                                       )
         results = AnalysisResults.model_validate_json(response.response)
     return results
 
-if 'is_admin' not in st.session_state:
-    st.session_state['is_admin'] = False
+def display_results(label, value):
+    st.write(f":orange[{label}] ", value)
+
+def display_key_topics(topics):
+    for topic in topics:
+        st.markdown("- " + topic)
+
+if 'IS_ADMIN' not in st.session_state:
+    st.session_state['IS_ADMIN'] = False
     
-if st.session_state['is_admin']:
+if st.session_state['IS_ADMIN']:
     if 'mongo_client' not in st.session_state:
         setup_mongo()
-    chat_collection = list(st.session_state['mongo_client'][os.environ["MONGODB_DB"]]['chat_session'].find())
-    
+    chat_ids = list(st.session_state['mongo_client'][st.session_state["MONGODB_DB"]]['chat_session'].find({}, {"_id": 1, "created_on": 1}))
+    chat_ids.reverse()
     with st.sidebar:
-        with st.expander('Chatlogs'):
-            for item in chat_collection:
-                if st.button(label = "Chatlog - "+str(item['_id'])) : 
-                    show_chat_details(item)
-            if 'inspecting_chat_collection' not in st.session_state:
-                show_chat_details(chat_collection[0])
+        setup_LLM()
+        '---'
+        st.write("Chatlogs")
+        for item in chat_ids:
+            if st.button(label = "Chatlog - "+item['created_on'].strftime("%Y-%b-%d %H:%M:%S"),use_container_width=True): 
+                show_chat_details(item)
+        if 'inspecting_chat_collection' not in st.session_state:
+            show_chat_details(chat_ids[0])
 
-
-    st.header(st.session_state['inspecting_chat_collection']['_id'])
+    st.header(st.session_state['inspecting_chat_collection']['created_on'].strftime("%Y-%B-%d %H:%M:%S"))
     with st.expander(label = "messages"):
         specific_chatlog = st.session_state['inspecting_chat_collection']['chatlog']
         for message in specific_chatlog:
             st.chat_message(name=message['role']).markdown(message['content'])
-            if 'intent' in message:
-                st.write("Intent: ", message['intent'])
+            if 'detected_user_intent' in message:
+                st.write("Intent: ", message['detected_user_intent'])
 
     st.header("Analysis")
     analysis_results = st.session_state['inspecting_chat_collection'].get('analysis_results', None)
     if analysis_results is not None:
         analysis_results = AnalysisResults.model_validate(analysis_results)
-    # st.write(st.session_state['inspecting_chat_collection']['_id'])
     if st.button(label = "Analyse Chatlog"):
-        if analysis_results is None:
+        if analysis_results is None and len(st.session_state['inspecting_chat_collection']['chatlog'])>2:
             analysis_results = analyse_chatlog(st.session_state['inspecting_chat_collection'])
             
             st.session_state['inspecting_chat_collection']['analysis_results'] = analysis_results
-            st.session_state['mongo_client'][os.environ["MONGODB_DB"]]['chat_session'].update_one({'_id': st.session_state['inspecting_chat_collection']['_id']},
+            st.session_state['mongo_client'][st.session_state["MONGODB_DB"]]['chat_session'].update_one({'_id': st.session_state['inspecting_chat_collection']['_id']},
                                                                                             {'$set':
                                                                                                 {'updated_on': datetime.datetime.now(),
-                                                                                                'is_analysed': True,
                                                                                                 'analysis_results': analysis_results.model_dump(mode='json')
                                                                                             }})
         else:
-            show_analysis_dialog()
+            show_analysis_dialog(st.session_state['inspecting_chat_collection'])
 
-    col1, col2 = st.columns(2)
-    if analysis_results is not None:
-        with col1:
-            st.write(":orange[Overall Sentiment:] ", analysis_results.overall_sentiment)
-            st.write(":orange[Interest in product:] ", analysis_results.interest_in_product)
-            st.write(":orange[Intent Classification Accuracy:] ", analysis_results.intent_classification_accuracy)
-            st.write(":orange[Conversation Turns:] ", analysis_results.conversation_turns)
-            st.write(":orange[Sign-up Disengagement Flag:] ", analysis_results.signup_disengagement_flag)
-        with col2:
-            st.write(":orange[Key Topics: ]")
-            for topic in analysis_results.key_topics:
-                st.markdown("- "+topic)
-            st.write(":orange[Sign-up Disengagement Reason:] ", analysis_results.signup_disengagement_reason)
-    else:
-        with col1:
-            st.write(":orange[Overall Sentiment:] ", None)
-            st.write(":orange[Interest in product:] ", None)
-            st.write(":orange[Intent Classification Accuracy:] ", None)
-            st.write(":orange[Conversation Turns:] ", None)
-            st.write(":orange[Disengagement Flag:] ", None)
-        with col2:
-            st.write(":orange[Key Topics:] ", None)
-            st.write(":orange[Disengagement Reason:] ", None)
+    left, right = st.columns(2)
+    with left:
+        display_results("Overall Sentiment", getattr(analysis_results, 'overall_user_sentiment', None))
+        display_results("Interest in product", getattr(analysis_results, 'interest_in_product', None))
+        display_results("Sign-up Disengagement Flag", getattr(analysis_results, 'is_user_not_interested_in_signing_up', None))
+        display_results("Interactions", int(st.session_state['inspecting_chat_collection'].get('interactions', 0)))
+    with right:
+        st.write(":orange[Key Topics:] ")
+        display_key_topics(getattr(analysis_results, 'key_topics', []))
+        display_results("Sign-up Disengagement Reason", getattr(analysis_results, 'disengagement_reason', None))
+
 
 else:
     def login():
-        st.session_state['is_admin'] = password==os.environ['ADMIN_PASSWORD']
+        st.session_state['IS_ADMIN'] = password == st.session_state['ADMIN_PASSWORD']
         
     with st.sidebar:
         password = st.text_input(label='Enter admin password', type='password')
